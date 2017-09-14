71af799fac74508ba9376f3a199a0e4f7a641940
user <dev@null.com>
2017-09-14 14:21:50 +1200

    replace the <aside> with a nav and ajax content in

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // js for prog web app talk
+
+// fetch nav sidebar and insert into page
+crispy.get('templates/nav.html')
+	.then(function(response) {
+		document.getElementsByTagName('nav')[0].innerHTML = response;
+	})
+	.catch(function(err) {
+		console.log("error fetching nav template: ", err);
+	});
