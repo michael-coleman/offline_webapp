0b5eb9c579a392f1c1e25eec8488b219fe707929
user <dev@null.com>
2017-09-18 10:29:09 +1200

    detect when "ready for offline" + display a notification

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// js for prog web app talk
+// vim: foldmethod=marker
 
 
 // test for - service worker - incompatible situations
@@ -7,15 +7,51 @@ if ( window.location.hostname !== "localhost" &&
 	alert('[NOTICE] https is required for service workers');
 }
 
+
+// Insert a notification box into the page - "ready for offline"
+function offline_ready() {
+	// create  notification box container
+	var notification = document.createElement('div');
+	 
+	// add styles and content to notification box
+	notification.className = "bg-grey-333 pam text-grey-eee";
+	notification.style.position = "fixed";
+	notification.style.bottom = "0%";
+	notification.innerHTML = 
+	`Service Worker install succeeded, app is offline ready
+	                                   <i class="fa fa-window-close mlm"></i>`;
+	 
+	// event handler to remove box when "close" icon clicked
+	notification.firstElementChild.addEventListener('click', function() {
+		notification.parentElement.removeChild(notification);
+	});
+	 
+	document.body.appendChild(notification);
+
+}
+
 if ('serviceWorker' in navigator) {
 	
 	// '/' is default, need to limit it to `prog_webapp_talk/` subdirectory
 	navigator.serviceWorker.register('service_worker.js', 
-		{ scope : '/offline_webapp/' })
+		                                     { scope : '/offline_webapp/' })
 	.then(function(registration) {
 		 
-		console.log('registration ok');
-		 
+		// onupdatefound fires ONLY when a new service worker is registered
+		registration.onupdatefound = function() {
+			
+			// get a reference to this new service worker and start tracking
+			// its progress
+			console.log( 'new service worker found...' );
+			var new_sw = registration.installing;
+			new_sw.onstatechange = function() {
+			
+			console.log( 'new sw state is: ' + new_sw.state );
+				if (new_sw.state === "installed") {
+					offline_ready();
+				}
+			};
+		};
 	})
 	.catch(function(err) {
 		 
